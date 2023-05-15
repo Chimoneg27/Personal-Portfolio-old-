6877e2af6e8624b161a55a6656b94fac67f6e221
Cosmas Wasswa <cosywas@gmail.com>
2023-05-15 12:17:18 +0300

    Added close functionality for mobile navigation items

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+}))
