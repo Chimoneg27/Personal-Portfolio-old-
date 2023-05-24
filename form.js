6c78731aa0242e2b652cc998e46dc79e015c24f8
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-05-24 20:21:09 +0630

    Adjusted styles for error message

--- form.js
+++ form.js
@@ -3,12 +3,15 @@ const emailInput = document.getElementById("email-input");
 const submitBtn = document.getElementById("form-btn");
 const errorMsg = document.getElementById("error-text");
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
     if (emailInput.value === emailInput.value.toLowerCase()) {
-      return true;
+        return true;
     } else {
-      errorMsg.style.display = "block";
-      event.preventDefault();
-      return false;
+        errorMsg.style.display = "block";
+        event.preventDefault();
+        return false;
     }
-  });
+});
+
+document.getElementById("error-text").style.color = "Red";
+document.getElementById("error-text").style.fontSize = "120%";
