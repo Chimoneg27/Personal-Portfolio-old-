7294c6a60fbcc322e3b5f26e82c18d1f692155fe
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-05-24 17:31:06 +0200

    removed unused variable

--- form.js
+++ form.js
@@ -1,17 +1,15 @@
-const form = document.getElementById("portfolio-form");
-const emailInput = document.getElementById("email-input");
-const submitBtn = document.getElementById("form-btn");
-const errorMsg = document.getElementById("error-text");
+const form = document.getElementById('portfolio-form');
+const emailInput = document.getElementById('email-input');
+const errorMsg = document.getElementById('error-text');
 
-form.addEventListener("submit", function (event) {
-    if (emailInput.value === emailInput.value.toLowerCase()) {
-        return true;
-    } else {
-        errorMsg.style.display = "block";
-        event.preventDefault();
-        return false;
-    }
+form.addEventListener('submit', (event) => {
+  if (emailInput.value === emailInput.value.toLowerCase()) {
+    return true;
+  }
+  errorMsg.style.display = 'block';
+  event.preventDefault();
+  return false;
 });
 
-document.getElementById("error-text").style.color = "Red";
-document.getElementById("error-text").style.fontSize = "120%";
+document.getElementById('error-text').style.color = 'Red';
+document.getElementById('error-text').style.fontSize = '120%';
