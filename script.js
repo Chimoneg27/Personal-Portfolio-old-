8d38ff1d33089c1b6e97baabed8cac14f2adb653
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-05-24 22:06:03 +0200

    Required changes made to input field in script.js

--- script.js
+++ script.js
@@ -14,24 +14,31 @@ document.querySelectorAll('.nav-link').forEach((n) => n
 
 const fullName = document.querySelector('#fullName');
 const email = document.querySelector('#email-input');
-const textInput = document.querySelector('#textArea');
+const textarea = document.getElementById('textArea');
 
 const savedInfo = JSON.parse(localStorage.getItem('myFormInfo'));
+
 if (savedInfo) {
   fullName.value = savedInfo.name;
   email.value = savedInfo.email;
-  textInput.value = savedInfo.message;
 }
 
 function saveFormInfo() {
   const myFormInfo = {
     name: fullName.value,
     email: email.value,
-    text: textInput.value,
   };
   localStorage.setItem('myFormInfo', JSON.stringify(myFormInfo));
 }
 
+if (localStorage.getItem('textareaData')) {
+  textarea.value = localStorage.getItem('textareaData');
+}
+
+textarea.addEventListener('input', () => {
+  const data = textarea.value;
+  localStorage.setItem('textareaData', data);
+});
+
 fullName.addEventListener('input', saveFormInfo);
 email.addEventListener('input', saveFormInfo);
-textInput.addEventListener('input', saveFormInfo);
