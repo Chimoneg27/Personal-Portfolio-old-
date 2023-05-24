129cffbe8b03d4dce372f59ec9f6dbf33a57d1c9
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-05-24 15:46:16 +0200

    Added validation function for form in form.js file

--- form.js
+++ form.js
@@ -2,3 +2,13 @@ const form = document.getElementById("portfolio-form");
 const emailInput = document.getElementById("email-input");
 const submitBtn = document.getElementById("form-btn");
 const errorMsg = document.getElementById("error-text");
+
+form.addEventListener("submit", function(event) {
+    if (emailInput.value === emailInput.value.toLowerCase()) {
+      return true;
+    } else {
+      errorMsg.style.display = "block";
+      event.preventDefault();
+      return false;
+    }
+  });
