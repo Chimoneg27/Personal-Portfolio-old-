5d9016b41b4c62c6d81b02193b4e2a62666e5b7a
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-05-24 20:13:16 +0630

    Linked form ids to form.js file

---
+++ form.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("portfolio-form");
+const emailInput = document.getElementById("email-input");
+const submitBtn = document.getElementById("form-btn");
+const errorMsg = document.getElementById("error-text");
